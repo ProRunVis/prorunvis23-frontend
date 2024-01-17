43ab715a4b43c05077c18d3554d76d3a58d7ed0e
David Martin Bessler <martin-bessler@gmx.de>
2024-01-17 18:04:28 +0100

    cleanup

--- src/ComponentsBeforeBackend/classComponents/LeftComponent.js
+++ src/ComponentsBeforeBackend/classComponents/LeftComponent.js
@@ -3,11 +3,11 @@ import React from 'react';
 // Die Hauptkomponente für die Dateiverarbeitung
 export default function LeftComponent() {
   return (
-    <main className="left-container white-text">
+    <main className="left-container ">
 
       {/* Benutzerdefinierte Schaltflächen für das Öffnen und Verarbeiten von Java-Dateien */}
       <div className="button-container">
-        <form className="text-box button" encType="multipart/form-data">
+        <form className="text-box" encType="multipart/form-data">
           <input type="file" name="file" multiple="" webkitdirectory="" />
         </form>
       </div>
