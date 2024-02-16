471e9da3d15935429217b1dea3d1ef43a2860194
fred <frederik@hinz-web.de>
2024-02-16 17:28:07 +0100

    Add TraceNode and TraceNodeHandler class

---
+++ src/Editor&Decoration/TraceNode.js
@@ -0,0 +1,22 @@
+class TraceNode{
+    constructor(typ, ranges, childs, parent, link, outlinks, out, iteration) {
+        this.typ = typ;
+        this.range = ranges;
+        this.childs = childs;
+        this.parent = parent;
+        this.link = link;
+        this.outlinks = outlinks;
+        this.out = out;
+        this.iteration = iteration;
+    }
+}
+
+class SourceRange {
+    constructor(startcol, endcol, startrow, endrow, file) {
+        this.startcol = startcol;
+        this.endcol = endcol;
+        this.startrow = startrow;
+        this.endrow = endrow;
+        this.file = file;
+    }
+}
