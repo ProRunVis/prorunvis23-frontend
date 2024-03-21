c9fbf1f849f792a658508f4b344bfc0f553fdc0b
Frederik Hinz <frederik@hinz-web.de>
2024-03-21 16:07:58 +0100

    adjust tutorial

--- src/WebsiteElements/ReadmeModal.js
+++ src/WebsiteElements/ReadmeModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+    import React from "react";
 import PropTypes from "prop-types";
 
 /**
@@ -18,22 +18,25 @@ export default function ReadmeModal({closeModal}) {
             </button>
             <div className="modal-content">
                 <h2>Tutorial</h2>
-                <p> Use the upload button to upload the root folder of a java project of your choosing. </p>
-                <p> The chosen project has to be in a separate folder with only the project in it. </p>
+                <p> Welcome to ProRunVis: </p>
+                <p> ProRunVis is a code analysis tool that is supposed to help you understand the control flow of your code through visualization. </p>
+                <p> Use the upload button to upload the root folder of a Java project of your choosing. </p>
+                <p> The chosen project has to be in a separate folder with only the project-packages in it. </p>
                 <p> It furthermore should not use any external libraries the program is currently not able to deal with
                     those. </p>
-                <p> Your code then gets shown on the right side. </p>
-                <p> Green background indicates that a statement has been executed. </p>
-                <p> Blue background indicates a jump at that position to another part of the code, click it to go
+                <p> The code of your project then gets shown on the right side. </p>
+                <p class=".active"> Green background indicates that a statement has been executed. </p>
+                <p class=".link"> Blue background indicates a jump at that position to another part of the code, click it to go
                     there. </p>
-                <p> Orange background indicates a loop that has been executed, click it to select the iteration that you
+                <p class=".loop"> Purple background indicates a loop that has been executed, click it to select the iteration that you
                     want displayed. </p>
+                <p class=".inactive"> Red background indicates that this is the last code segment that got executed. </p>
                 <p> The line on the left side of your code is shown to indicate what path the program took, so where it
                     started and where it ended. </p>
             </div>
         </div>
     );
 }
-ReadmeModal.propTypes = {
-    closeModal: PropTypes.instanceOf(Function)
-};
+    ReadmeModal.propTypes = {
+        closeModal: PropTypes.instanceOf(Function)
+    };
